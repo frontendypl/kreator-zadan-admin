f4dd9d47e3ce55e1f5a592b9256fecdf81df1b22
MichalK <frontendypl@gmail.com>
2022-07-05 12:43:47 +0200

    login view and  methods

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import AuthRegisterView from '../views/AuthRegisterView'
+import AuthRegisterView from '@/views/AuthRegisterView'
+import AuthLoginView from "@/views/AuthLoginView";
 
 Vue.use(VueRouter)
 
@@ -9,7 +10,17 @@ const routes = [
   {
     path: '/',
     name: 'home',
+    component: AuthRegisterView //TODO zrobić widok dla zalogowanego, przekierowywac jesli niezalogowany
+  },
+  {
+    path: '/rejestracja',
+    name: 'AuthRegisterView',
     component: AuthRegisterView
+  },
+  {
+    path: '/logowanie',
+    name: 'AuthLoginView',
+    component: AuthLoginView
   }
 ]
 
---
+++ src/views/AuthLoginView.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="v-AuthLoginView w-100 my-auto">
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <form class="mx-auto" @submit.prevent="handleForm">
+            <div class="row">
+              <div class="col">
+                <h1>Zaloguj się:</h1>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <input type="text" class="form-control form-control-lg w-100 fs-1 mb-2" placeholder="Email"
+                       v-model="user.email"
+                >
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <input type="password" class="form-control form-control-lg w-100 fs-1 mb-2" placeholder="Hasło"
+                       v-model="user.password"
+                >
+              </div>
+            </div>
+             <div class="row">
+              <div class="col">
+                <button class="btn btn-primary text-light btn-lg w-100">
+                  <i class="bi bi-check fs-1"></i>
+                </button>
+                <h2 v-for="(error, key, i) in errors" :key="i" :data-test="i">{{error.message}}</h2>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters, mapActions} from 'vuex'
+import axios from 'axios'
+
+export default {
+  name: "AuthLoginView",
+  data(){
+    return {
+      user: {
+        email: '',
+        password: '',
+      },
+      errors: {
+        required: {
+          message: ''
+        }
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(['apiUrl'])
+  },
+  methods: {
+    ...mapActions(['setUser']),
+    /**
+     *
+     */
+    validation(){
+      this.errors = {
+        required: {
+          message: ''
+        }
+      }
+      if(!this.user.email || !this.user.password){
+        this.errors.required.message = 'Uzupełnij wszystkie pola'
+        return false
+      }
+
+      return true
+    },
+    /**
+     *
+     */
+    async handleForm(){
+
+      if(!this.validation()) return false
+
+      try{
+        const response = await axios.post(`${this.apiUrl}/users/login`, this.user)
+        this.setUser({...response.data.user, token: response.data.token})
+      }catch (e) {
+        console.log({e})
+        this.errors = e.response.data.errors
+      }
+
+    }
+  }
+}
+</script>
