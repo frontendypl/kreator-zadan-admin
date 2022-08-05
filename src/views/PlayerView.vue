741687e930ca9d21715d4c86c27ec31aebff55f8
MichalK <frontendypl@gmail.com>
2022-08-05 18:41:53 +0200

    show player name in PlayerViews

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="v-PlayerView">
-    Player View:
+    <h4 class="mb-3" v-if="playerData"><b>{{playerData.name}} </b>, {{activeList.name}}</h4>
     <AnswersComponent :answers="playerAnswers" :userImages="userImages"/>
   </div>
 </template>
 
 <script>
 import AnswersComponent from "@/components/AnswersComponent";
-import {mapActions, mapState} from "vuex";
+import {mapActions, mapGetters, mapState} from "vuex";
 export default {
   name: 'PlayerView',
   components: {AnswersComponent},
@@ -21,8 +21,15 @@ export default {
       listId: state => state.listModule.listId,
       answers: state=>state.answerModule.answers,
       userImages: state => state.imageModule.userImages,
+      players: state => state.playerModule.players,
+      ...mapGetters({
+        activeList: 'listModule/activeList'
+      }),
       playerAnswers(){
         return this.answers.filter(answer=>answer.player._id === this.$route.params.playerId)
+      },
+      playerData(){
+        return this.players.find(player=>player._id === this.$route.params.playerId)
       }
     })
   },
@@ -30,17 +37,21 @@ export default {
     ...mapActions({
       setBackPathObject: 'setBackPathObject',
       getAnswers: 'answerModule/getAnswers',
+      getPlayers: 'playerModule/getPlayers'
     }),
 
   },
   created(){
     this.getAnswers()
+    if(!this.players.length){
+      this.getPlayers()
+    }
   },
   mounted(){
     this.setBackPathObject({name: 'ExerciseListView', params: {listId: this.listId}})
     this.interval = setInterval(()=>{
       this.getAnswers()
-    },5000)
+    },3000)
   },
   beforeDestroy() {
     clearInterval(this.interval)
